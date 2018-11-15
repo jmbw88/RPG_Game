410f89c106c234df86a202c0179ed911c37e5294
jmbw1988 <jmbw1988@gmail.com>
2018-11-14 19:45:16 -0500

    update 11.14

---
+++ Assets/Javascript.js
@@ -0,0 +1,28 @@
+// Define character objects
+
+var obiWan = {
+    attack: 10,
+    health: 100,
+    counterAttack: 10
+}
+
+var darthVader = {
+    attack: 12,
+    health: 120,
+    counterAttack: 12
+}
+
+var emperorPalp = {
+    attack: 15,
+    health: 135,
+    counterAttack: 15
+}
+
+var masterYoda = {
+    attack: 18,
+    health: 150,
+    counterAttack: 18
+}
+
+document.getElementById("obiHealth").innerHTML=[obiWan.health]
+document.getElementById("obiAttack").innerHTML=[obiWan.attack]
